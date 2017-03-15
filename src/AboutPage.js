10f3b990fd4766538878e6df400932af9a2c91b6
sumeetv <suvaidya@gmail.com>
2017-03-14 19:44:23 -0700

    Update style to reflect eslint standards
    Summary: Clean up some of the UI while cleaning up the strings
    and general code
    
    Test Plan: View site
    
    Author: sumeetv
    
    Reviewers:

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -2,21 +2,43 @@ import React, { Component } from 'react';
 
 class AboutPage extends Component {
   render() {
-    console.log("rendering the about page");
     return (
       <div>
         <h1>Work</h1>
         <h2>Uber</h2>
         <p>
-          I am currently an engineering manager on the Fleets team at Uber. Our team's goal is to build the products and tools that support people who own multiple cars and want to have a side (or main!) business using Uber. Before the Fleets team, I worked on India Growth, focusing on the driver funnel in that region as both a server-side engineer and an engineering manager.
+          {"I am currently an engineering manager on the \
+            Fleets team at Uber. Our team's goal is to build \
+            the products and tools that support people who own \
+            multiple cars and want to have a side (or main!) \
+            business using Uber. Before the Fleets team, I \
+            worked on India Growth, focusing on the driver \
+            funnel in that region as both a server-side \
+            engineer and an engineering manager."}
         </p>
         <h2>Facebook</h2>
         <p>
-          I started working at Facebook on the follow features back when Facebook, Google+, and Twitter were competing in the social media wars of 2011. After our launch, I worked briefly on a team that owned privacy, sharing, the composer, follow, and lists. Highlights included launching interest lists and fixing privacy issues to prevent multi-million dollar fines imposed by the Irish Data Protection Commissioner. I also spent a few years working on the Groups team where I worked as a backend engineer on core functionality before launching the iOS Groups app as the primary React Native engineer.
+          {"I started working at Facebook on the follow \
+            features back when Facebook, Google+, and Twitter \
+            were competing in the social media wars of 2011. \
+            After our launch, I worked briefly on a team that \
+            owned privacy, sharing, the composer, follow, and \
+            lists. Highlights included launching interest lists \
+            and fixing privacy issues to prevent major dollar \
+            fines imposed by international privacy reviews. I \
+            also spent a few years working on the Groups team \
+            where I worked as a backend engineer on core \
+            functionality before launching the iOS Groups app \
+            as the primary React Native engineer."}
         </p>
         <h2>Other</h2>
         <p>
-          Before working at Facebook, I was an student at Michigan majoring in computer science engineering with a minor in ethical and political philosophy. I had a few internships at Microsoft, Autodesk, and IBM (business consulting!) and had a brief stint competing in ballroom dancing.
+          {"Before working at Facebook, I was an student at \
+            Michigan majoring in computer science engineering \
+            with a minor in ethical and political philosophy. I \
+            had a few internships at Microsoft, Autodesk, and \
+            IBM (business consulting!) and had a brief stint \
+            competing in ballroom dancing."}
         </p>
       </div>
     );
--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import {
+  List,
+  ListItem
+} from 'material-ui/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {
   BrowserRouter as Router,
@@ -14,23 +18,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  renderHeader() {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-title-container">
+          <h2 className="App-title">
+            {"Sumeet is an engineering manager in San Francisco \
+              with too many interests and not enough time"}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+
+  renderNav() {
+    return(
+      <List className="App-index">
+        <ListItem 
+          primaryText="About"
+          containerElement={<Link to="/about" />} />
+        <ListItem primaryText="Blog" />
+        <ListItem primaryText="Personal Projects" />
+        <ListItem primaryText="Scotch" />
+        <ListItem
+          primaryText="Games"
+          containerElement={<Link to="/games" />} />
+      </List>
+    );
+  }
+
   render() {
     return (
       <Router>
         <MuiThemeProvider>
           <div className="App">
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>{"Welcome to sumeetv's clownshow!"}</h2>
-            </div>
+            {this.renderHeader()}
             <div className="App-container">
-              <div className="App-index">
-                <Link to="/about">About</Link>
-                <p>Blog</p>
-                <p>Personal Projects</p>
-                <p>Scotch</p>
-                <Link to="/games">Games</Link>
-              </div>
+              {this.renderNav()}
               <div className="App-content">
                 <Route exact path="/" component={AboutPage} />
                 <Route path="/about" component={AboutPage} />
